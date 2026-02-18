183b29131ec2c5d5466b2b9aaff66fa65b968ff6
ericklesterdev <ericklester.dev@gmail.com>
2026-02-18 19:25:56 +0800

    feat: add nba-scores-sdk with embeddable live scores widget

---
+++ sdk/nba-scores-sdk/nba-scores-sdk/src/main/kotlin/com/nbahub/sdk/scores/TeamScoresWidget.kt
@@ -0,0 +1,16 @@
+package com.nbahub.sdk.scores
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.nbahub.feature.scores.ScoresScreen
+import com.nbahub.platform.design.NbaHubTheme
+
+@Composable
+fun TeamScoresWidget(teamId: Int, modifier: Modifier = Modifier) {
+    NbaHubTheme {
+        ScoresScreen(
+            dependencies = NbaSDK.container.scoresFeatureDependencies,
+            modifier = modifier,
+        )
+    }
+}
